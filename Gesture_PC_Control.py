f70062c35e646ae2ac003a2609d2852709a961cc
Crossword88 <shmakovsergej760@gmail.com>
2026-06-07 09:37:38 +0300

    single gesture mode

--- Gesture_PC_Control.py
+++ Gesture_PC_Control.py
@@ -13,9 +13,20 @@ import numpy as np
 import threading
 import ctypes
 import time
+import win32api
 import sys
 from pathlib import Path
 import threading
+
+import ctypes
+
+class POINT(ctypes.Structure):
+    _fields_ = [("x", ctypes.c_long), ("y", ctypes.c_long)]
+
+def get_mouse_pos():
+    pt = POINT()
+    ctypes.windll.user32.GetCursorPos(ctypes.byref(pt))
+    return pt.x, pt.y
 
 VOLUME_GESTURE = 0
 RIGHT_MOUSE = 1
@@ -29,13 +40,14 @@ MOUSEEVENTF_MOVE = 0x0001
 MOUSEEVENTF_LEFTDOWN = 0x0002
 MOUSEEVENTF_LEFTUP = 0x0004
 
-model = YOLO(r"model.pt")
+model = YOLO(r"C:\Models\Gesture_Controller_YOLO\Gesture_Model_26n13\weights\best.engine")
 
-def show():                                     #showing the webcam frame with model predictions
+def show():
     while True:
         if window_frame is None: continue
 
         final_frame = window_frame.copy()
+
 
         rgb = 0, 0, 0
         if CLASS == 0: rgb = 0,255,0
@@ -44,12 +56,14 @@ def show():                                     #showing the webcam frame with m
         elif CLASS == 3: rgb = 0,0,255
         elif CLASS == 4: rgb = 100,115,0
 
+
         if CLASS is not None:
             label = f"{model.names[int(CLASS)]} {float(CONFIDENCE):.2f}"
             x1, y1, x2, y2 = [int(v.cpu().item()) for v in BBOX]
             cv2.rectangle(final_frame, (width - x1, y1), (width - x2, y2), (rgb), 2)      #flipped frame, convert box to the other side
             cv2.putText(final_frame, label, (width - x1, y1 - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (rgb), 2)
+
 
         cv2.imshow("cam", final_frame)
         c = cv2.waitKey(1)
@@ -58,57 +72,24 @@ def show():                                     #showing the webcam frame with m
 
     cv2.destroyAllWindows()
 
-def controller():                                #entire code for mouse & volume control
+
+
+def controller():
     while True:
         sleep(0.0001)
-        if CLASS == VOLUME_GESTURE:
-            if gesture_center < 350:
-                win32api.keybd_event(win32con.VK_VOLUME_UP, 0)
-                win32api.keybd_event(win32con.VK_VOLUME_UP, 0, win32con.KEYEVENTF_KEYUP)
-                sleep(0.05)
-            else:
-                win32api.keybd_event(win32con.VK_VOLUME_DOWN, 0)
-                win32api.keybd_event(win32con.VK_VOLUME_DOWN, 0, win32con.KEYEVENTF_KEYUP)
-                sleep(0.05)
 
-        elif CLASS == RIGHT_MOUSE:
-            ctypes.windll.user32.mouse_event(
-                MOUSEEVENTF_MOVE,
-                1,
-                0,
-                0,
-                0
-            )
-        elif CLASS == UP_MOUSE:
-            ctypes.windll.user32.mouse_event(
-                MOUSEEVENTF_MOVE,
-                0,
-                -1,
-                0,
-                0
-            )
-        elif CLASS == LEFT_MOUSE:
-            ctypes.windll.user32.mouse_event(
-                MOUSEEVENTF_MOVE,
-                -1,
-                0,
-                0,
-                0
-            )
-        elif CLASS == DOWN_MOUSE:
-            ctypes.windll.user32.mouse_event(
-                MOUSEEVENTF_MOVE,
-                0,
-                1,
-                0,
-                0
-            )
+        if CLASS == VOLUME_GESTURE:
+            ctypes.windll.user32.SetCursorPos(int(1400-(int(gesture_center_X) * 1.25)), int(gesture_center_Y) + 10)        #simle formula, there is ~1400px for max X mouse coordinate, and ~1200px for gesture max center X coordinate
+
         elif CLASS == CLICK:
             fast_click()
             sleep(0.3)
 
+
+
 def fast_click():
     ctypes.windll.user32.mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+    # Минимальная пауза, чтобы игра засчитала клик
     ctypes.windll.user32.mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
 
 def watcher():
@@ -124,30 +105,46 @@ def watcher():
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+    print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # реальная ширина
+    print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # реальная высота
+
+    # camering = threading.Thread(target = shower, args=(cap,), daemon=True)
+    # camering.start()
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('output.mp4', fourcc, 30, (width, height))
+
     Thread(target=show, args=(), daemon=True).start()
 
     Thread(target=controller, args=(), daemon=True).start()
 
-    global gesture_center
+    global gesture_center_X, gesture_center_Y
 
     while True:
         ret, frame = cap.read()
         frame_to_model = cv2.cvtColor(np.array(frame,), cv2.COLOR_BGRA2BGR)
+        #out.write(frame)
         window_frame = cv2.flip(frame, 1)
 
         CLASS = None
 
-        result = model.predict(frame_to_model, conf = 0.4, device = 'cpu', verbose = False)[0]
+        result = model.predict(frame_to_model, conf = 0.4, device = 'cuda', verbose = False)[0]
         for box, cls, conf  in zip(result.boxes.xyxy, result.boxes.cls, result.boxes.conf):
             x1, y1, x2, y2 = box
 
             if cls == VOLUME_GESTURE:
-                gesture_center = (y1 + y2) / 2
+                gesture_center_Y = (y1 + y2) / 2
+                gesture_center_X = (x1 + x2) / 2
+                # print("X: ", int(gesture_center_X), " Y: ", int(gesture_center_Y))
+                # print("Mouse: ", get_mouse_pos())
+
+
 
             CLASS = cls
             BBOX = box
             CONFIDENCE = conf
 
+    out.release()
     cap.release()
 
 
